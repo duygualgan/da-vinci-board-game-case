40fdcbb21ffd4df2a02b4d1fbe8476c269db0d6e
Duygu Algan <duygu.algan.esk26@gmail.com>
2025-09-08 13:17:53 +0300

    #main
    fix: vercel bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
-  //  server: {
-  //   historyApiFallback: true
-  // },
    build: {
     outDir: "dist",
-    rollupOptions: {
-      input: "/index.html"
-    }
+   
   },
 
 })
